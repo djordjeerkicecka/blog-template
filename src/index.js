53887594460ac6749dbe6f4f055d142c1b1385da
Djole <erkicdjole@gmail.com>
2022-12-12 12:36:40 +0100

    Project cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+// Modules
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// Components
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
